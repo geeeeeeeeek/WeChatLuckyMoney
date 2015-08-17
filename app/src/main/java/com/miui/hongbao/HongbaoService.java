7eaa5dc46c51ffc3f51a338bdbcea15051b3238a
ZhongyiTong <zhongyi@teambition.com>
2015-08-17 17:20:32 +0800

    取消按红包名称匹配，可以抢会话中最新的一个红包。但是还没有做已经抢过的红包的记录，会不断点击，场面暴力，有待优化。

--- app/src/main/java/com/miui/hongbao/HongbaoService.java
+++ app/src/main/java/com/miui/hongbao/HongbaoService.java
@@ -1,8 +1,13 @@
 package com.miui.hongbao;
 
 import android.accessibilityservice.AccessibilityService;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,9 +19,9 @@ public class HongbaoService extends AccessibilityService {
     private boolean mLuckyMoneyReceived;
     private boolean mNeedUnpack;
     private boolean mNeedBack = false;
-    private int mLuckyMoneyCount = 1;
-    private String mNextName = "红包1";
+    private List<String> fetchIdentifiers = new ArrayList<>();
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
 
@@ -29,15 +34,17 @@ public class HongbaoService extends AccessibilityService {
                 mReiceiveNode = null;
                 mUnpackNode = null;
                 checkNodeInfo();
+                /* 如果已经接收到红包并且还没有戳开 */
                 if (mLuckyMoneyReceived && !mLuckyMoneyPicked && (mReiceiveNode != null)) {
                     int size = mReiceiveNode.size();
                     if (size > 0) {
-                        AccessibilityNodeInfo cellNode = mReiceiveNode.get(size-1);
+                        AccessibilityNodeInfo cellNode = mReiceiveNode.get(size - 1);
                         cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
                         mLuckyMoneyReceived = false;
                         mLuckyMoneyPicked = true;
                     }
                 }
+                /* 如果戳开但还未领取 */
                 if (mNeedUnpack && (mUnpackNode != null)) {
                     int size = mUnpackNode.size();
                     if (size > 0) {
@@ -49,30 +56,38 @@ public class HongbaoService extends AccessibilityService {
             }
 
             if (mNeedBack) {
-                try {
-                    Thread.sleep(3000);
-                    performGlobalAction(GLOBAL_ACTION_BACK);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                performGlobalAction(GLOBAL_ACTION_BACK);
                 mNeedBack = false;
             }
         }
     }
 
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    /**
+     * 检查节点信息
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void checkNodeInfo() {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
 
         if (nodeInfo != null) {
-            List<AccessibilityNodeInfo> node1 = nodeInfo.findAccessibilityNodeInfosByText(mNextName);
+            /* 聊天会话窗口，遍历节点匹配“领取红包” */
+            List<AccessibilityNodeInfo> node1 = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
             if (!node1.isEmpty()) {
-                mLuckyMoneyReceived = true;
-                mReiceiveNode = node1;
-                mLuckyMoneyCount++;
-                mNextName = "红包" + mLuckyMoneyCount;
+                String nodeId = Integer.toHexString(System.identityHashCode(nodeInfo));
+                Log.d("233",nodeInfo.toString());
+                if (!checkFetched(nodeId)){
+                    mLuckyMoneyReceived = true;
+                    mReiceiveNode = node1;
+                }
                 return;
             }
 
+            /* 戳开红包，红包还没抢完，遍历节点匹配“拆红包” */
             List<AccessibilityNodeInfo> node2 = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
             if (!node2.isEmpty()) {
                 mUnpackNode = node2;
@@ -80,6 +95,7 @@ public class HongbaoService extends AccessibilityService {
                 return;
             }
 
+            /* 戳开红包，红包已被抢完，遍历节点匹配“红包详情”和“手慢了” */
             if (mLuckyMoneyPicked) {
                 List<AccessibilityNodeInfo> node3 = nodeInfo.findAccessibilityNodeInfosByText("红包详情");
                 List<AccessibilityNodeInfo> node4 = nodeInfo.findAccessibilityNodeInfosByText("手慢了");
@@ -92,9 +108,13 @@ public class HongbaoService extends AccessibilityService {
 
     }
 
-    @Override
-    public void onInterrupt() {
-
+    private boolean checkFetched(String nodeId) {
+        for (String identifier : fetchIdentifiers) {
+            if (nodeId.equals(identifier))
+                return true;
+        }
+        fetchIdentifiers.add(nodeId);
+        return false;
     }
 
 }
