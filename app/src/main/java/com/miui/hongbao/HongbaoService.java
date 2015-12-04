63ffc4355c1f87645478eb57cc30c22e1749d341
ZhongyiTong <zhongyi@teambition.com>
2015-12-04 23:01:10 +0800

    代码重构.将自己发的红包也加入了抢的范围.

--- app/src/main/java/com/miui/hongbao/HongbaoService.java
+++ app/src/main/java/com/miui/hongbao/HongbaoService.java
@@ -3,9 +3,6 @@ package com.miui.hongbao;
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.TargetApi;
 import android.os.Build;
-import android.os.PowerManager;
-import android.util.Log;
-import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -16,65 +13,63 @@ import java.util.regex.Pattern;
 
 
 public class HongbaoService extends AccessibilityService {
-    private List<AccessibilityNodeInfo> mReiceiveNode = null;
+    private List<AccessibilityNodeInfo> mReceiveNode = null;
     private List<AccessibilityNodeInfo> mUnpackNode = null;
 
     private boolean mLuckyMoneyPicked;
     private boolean mLuckyMoneyReceived;
     private boolean mNeedUnpack;
     private boolean mNeedBack = false;
+
     private List<String> fetchIdentifiers = new ArrayList<>();
     private String lastFetchedHongbaoId = null;
-    private long lastFetchedtime = System.currentTimeMillis();
+    private long lastFetchedTime = 0;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        AccessibilityNodeInfo nodeInfo = event.getSource();
 
-        final int eventType = event.getEventType();
+        if (nodeInfo == null) return;
 
-        if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED || eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
-            AccessibilityNodeInfo nodeInfo = event.getSource();
+        mReceiveNode = null;
+        mUnpackNode = null;
+        checkNodeInfo();
 
-            if (null != nodeInfo) {
-                mReiceiveNode = null;
-                mUnpackNode = null;
-                checkNodeInfo();
-                /* 如果已经接收到红包并且还没有戳开 */
-                if (mLuckyMoneyReceived && !mLuckyMoneyPicked && (mReiceiveNode != null)) {
-                    int size = mReiceiveNode.size();
-                    if (size > 0) {
+        /* 如果已经接收到红包并且还没有戳开 */
+        if (mLuckyMoneyReceived && !mLuckyMoneyPicked && (mReceiveNode != null)) {
+            int size = mReceiveNode.size();
+            if (size > 0) {
 
-                        String id = getHongbaoHash(mReiceiveNode.get(size - 1));
+                String id = getHongbaoHash(mReceiveNode.get(size - 1));
 
-                        long now = System.currentTimeMillis();
-                        if (id == null || (now - lastFetchedtime < 5000) && id.equals(lastFetchedHongbaoId))
-                            return;
+                long now = System.currentTimeMillis();
+                if (id == null || (now - lastFetchedTime < 5000) && id.equals(lastFetchedHongbaoId))
+                    return;
 
-                        lastFetchedHongbaoId = id;
-                        lastFetchedtime = now;
+                lastFetchedHongbaoId = id;
+                lastFetchedTime = now;
 
-                        AccessibilityNodeInfo cellNode = mReiceiveNode.get(size - 1);
-                        cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                        mLuckyMoneyReceived = false;
-                        mLuckyMoneyPicked = true;
-                    }
-                }
-                /* 如果戳开但还未领取 */
-                if (mNeedUnpack && (mUnpackNode != null)) {
-                    int size = mUnpackNode.size();
-                    if (size > 0) {
-                        AccessibilityNodeInfo cellNode = mUnpackNode.get(size - 1);
-                        cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                        mNeedUnpack = false;
-                    }
-                }
+                AccessibilityNodeInfo cellNode = mReceiveNode.get(size - 1);
+                cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                mLuckyMoneyReceived = false;
+                mLuckyMoneyPicked = true;
             }
-
-            if (mNeedBack) {
-                performGlobalAction(GLOBAL_ACTION_BACK);
-                mNeedBack = false;
+        }
+        /* 如果戳开但还未领取 */
+        if (mNeedUnpack && (mUnpackNode != null)) {
+            int size = mUnpackNode.size();
+            if (size > 0) {
+                AccessibilityNodeInfo cellNode = mUnpackNode.get(size - 1);
+                cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                mNeedUnpack = false;
             }
+        }
+
+
+        if (mNeedBack) {
+            performGlobalAction(GLOBAL_ACTION_BACK);
+            mNeedBack = false;
         }
     }
 
@@ -90,39 +85,40 @@ public class HongbaoService extends AccessibilityService {
     private void checkNodeInfo() {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
 
-        if (nodeInfo != null) {
-            /* 聊天会话窗口，遍历节点匹配“领取红包” */
-            List<AccessibilityNodeInfo> node1 = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
-            if (!node1.isEmpty()) {
-                String nodeId = Integer.toHexString(System.identityHashCode(nodeInfo));
-                Log.d("233", nodeInfo.toString());
-                if (!checkFetched(nodeId)) {
-                    mLuckyMoneyReceived = true;
-                    mReiceiveNode = node1;
-                }
-                return;
-            }
+        if (nodeInfo == null) return;
 
-            /* 戳开红包，红包还没抢完，遍历节点匹配“拆红包” */
-            List<AccessibilityNodeInfo> node2 = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
-            if (!node2.isEmpty()) {
-                mUnpackNode = node2;
-                mNeedUnpack = true;
-                return;
-            }
+        /* 聊天会话窗口，遍历节点匹配“领取红包”和"查看红包" */
+        List<AccessibilityNodeInfo> node0 = nodeInfo.findAccessibilityNodeInfosByText("查看红包");
+        List<AccessibilityNodeInfo> node1 = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
 
-            /* 戳开红包，红包已被抢完，遍历节点匹配“红包详情”和“手慢了” */
-            if (mLuckyMoneyPicked) {
-                List<AccessibilityNodeInfo> node3 = nodeInfo.findAccessibilityNodeInfosByText("红包详情");
-                List<AccessibilityNodeInfo> node4 = nodeInfo.findAccessibilityNodeInfosByText("手慢了");
-                if (!node3.isEmpty() || !node4.isEmpty()) {
-                    mNeedBack = true;
-                    mLuckyMoneyPicked = false;
-                }
+        if (!node1.isEmpty() || !node0.isEmpty()) {
+            String nodeId = Integer.toHexString(System.identityHashCode(nodeInfo));
+            if (!checkFetched(nodeId)) {
+                mLuckyMoneyReceived = true;
+                mReceiveNode = node1.isEmpty() ? node1 : node0;
             }
+            return;
         }
 
+        /* 戳开红包，红包还没抢完，遍历节点匹配“拆红包” */
+        List<AccessibilityNodeInfo> node2 = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
+        if (!node2.isEmpty()) {
+            mUnpackNode = node2;
+            mNeedUnpack = true;
+            return;
+        }
+
+        /* 戳开红包，红包已被抢完，遍历节点匹配“红包详情”和“手慢了” */
+        if (mLuckyMoneyPicked) {
+            List<AccessibilityNodeInfo> node3 = nodeInfo.findAccessibilityNodeInfosByText("红包详情");
+            List<AccessibilityNodeInfo> node4 = nodeInfo.findAccessibilityNodeInfosByText("手慢了");
+            if (!node3.isEmpty() || !node4.isEmpty()) {
+                mNeedBack = true;
+                mLuckyMoneyPicked = false;
+            }
+        }
     }
+
 
     private boolean checkFetched(String nodeId) {
         for (String identifier : fetchIdentifiers) {
